5e36db76c84ca785a2934ab52102af2d305d23f3
Alex Kinney <almiki@gmail.com>
2013-02-26 15:10:01 -0500

    Applied rounding fixes to non-C compress() method.

--- polycomp/codec.py
+++ polycomp/codec.py
@@ -21,8 +21,8 @@ def compress(polyline, precision=5):
     prev_y = 0
 
     for point in polyline:
-        x_trunc = int(point[0] * precision)
-        y_trunc = int(point[1] * precision)
+        x_trunc = long(round(point[0] * precision))
+        y_trunc = long(round(point[1] * precision))
 
         # Encode the difference between the coordinates.
         compressed.append(encode_number(x_trunc - prev_x))
